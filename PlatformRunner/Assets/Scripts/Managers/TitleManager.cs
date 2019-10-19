8593f6a6d8b77fb9bacc49f5ecdb45d14f6bd18f
cheonjaewoong <cheonjaewoong@gmail.com>
2019-10-19 13:34:38 +0900

    Delete GamaManager and create managers on each scenes

--- PlatformRunner/Assets/Scripts/Managers/TitleManager.cs
+++ PlatformRunner/Assets/Scripts/Managers/TitleManager.cs
@@ -4,23 +4,15 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 /*
- * GameManager 스크립트
- * 게임의 화면설정과 씬 내의 UI 행동 정의
+ * TitleScene 관리 스크립트
+ * 게임 화면설정 및 UI 컨트롤
  */
-public class GameManager : MonoBehaviour
+public class TitleManager : MonoBehaviour
 {
-    public static GameManager instance;
-    //게임 종료를 다시 묻는 패널
     public GameObject QuitPanel;
 
     private void Awake()
     {
-        //게임매니저 스크립트는 싱글톤으로 만들어 게임 전체에서 사용
-        if (instance == null)
-            instance = this;
-        //씬을 이동해도 파괴되지 않도록 설정
-        DontDestroyOnLoad(instance);
-
         //화면 해상도를 고정시키고 게임 도중 화면이 꺼지지 않도록 설정
         Screen.SetResolution(1920, 1080, true);
         Screen.sleepTimeout = SleepTimeout.NeverSleep;
@@ -37,30 +29,30 @@ public class GameManager : MonoBehaviour
         }
     }
 
-    /* 게임종료 관련 UI 컨트롤 */
-    //QuitButton의 행동
+    /* QuitPanel UI 컨트롤 */
+    //QuitButton
     public void QuitButton()
     {
         //Panel을 띄움
         QuitPanel.SetActive(true);
     }
 
-    //YesButton의 행동
+    //YesButton
     public void YesButton()
     {
         //게임 종료
         Application.Quit();
     }
 
-    //NoButton의 행동
+    //NoButton
     public void NoButton()
     {
         //Panel을 숨김
         QuitPanel.SetActive(false);
     }
 
-    /* Title Scene의 UI 컨트롤 */
-    //StartButton의 행동
+    /* TitleScene UI 컨트롤 */
+    //StartButton
     public void StartButton()
     {
         //Stage Scene 로드
