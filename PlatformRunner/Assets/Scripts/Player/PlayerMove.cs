09c8d5a35e5d2eac5638c7c2eeb7f02bd1bc74bf
cheonjaewoong <cheonjaewoong@gmail.com>
2019-10-19 17:50:36 +0900

    Create player moving animations

--- PlatformRunner/Assets/Scripts/Player/PlayerMove.cs
+++ PlatformRunner/Assets/Scripts/Player/PlayerMove.cs
@@ -9,11 +9,12 @@ public class PlayerMove : MonoBehaviour
 {
     private Rigidbody2D rigid;
     private SpriteRenderer sprite;
-    private PlayerAnimation animation;
+    private PlayerAnimation animationScript;
 
     //이동 제어 변수
     private int moveDir;
     private bool isJumping;
+    private bool isAir;
 
     //이동 수치 변수
     private float speed;
@@ -24,10 +25,11 @@ public class PlayerMove : MonoBehaviour
     {
         rigid = gameObject.GetComponent<Rigidbody2D>();
         sprite = gameObject.GetComponent<SpriteRenderer>();
-        animation = gameObject.GetComponent<PlayerAnimation>();
+        animationScript = gameObject.GetComponent<PlayerAnimation>();
 
         moveDir = 0;
         isJumping = false;
+        isAir = true;
 
         speed = 50.0f;
         maxSpeed = 5.0f;
@@ -41,7 +43,7 @@ public class PlayerMove : MonoBehaviour
         if(moveDir != 0)
         {
             rigid.AddForce(Vector2.right * moveDir * speed * Time.deltaTime, ForceMode2D.Impulse);
-            animation.SetAnimationRun(true);
+            animationScript.SetAnimationRun(true);
             if(moveDir == -1)
             {
                 sprite.flipX = true;
@@ -64,8 +66,16 @@ public class PlayerMove : MonoBehaviour
         if (isJumping)
         {
             rigid.AddForce(Vector2.up * jumpPower * Time.deltaTime, ForceMode2D.Impulse);
+            animationScript.SetAnimationJumpStart();
             isJumping = false;
         }
+    }
+
+    //충돌시 다시 점프가 가능하도록 변경
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        animationScript.SetAnimationJumpStop();
+        isAir = false;
     }
 
     /* UI의 이동 버튼 컨트롤 */
@@ -82,14 +92,16 @@ public class PlayerMove : MonoBehaviour
     public void MoveButtonUp()
     {
         rigid.velocity = new Vector2(0, rigid.velocity.y);
-        animation.SetAnimationRun(false);
+        animationScript.SetAnimationRun(false);
         moveDir = 0;
     }
 
     public void JumpButton()
     {
-        if (!isJumping)
+        //공중에 있지 않을 때만 점프 가능
+        if (!isAir)
         {
+            isAir = true;
             isJumping = true;
         }
         else
