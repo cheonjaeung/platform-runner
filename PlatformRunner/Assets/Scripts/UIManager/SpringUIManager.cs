e2677bf9c717840a5ea8bb48992c57bf8cfd1be2
cheonjaewoong <cheonjaewoong@gmail.com>
2019-10-20 20:41:06 +0900

    Create stage spring-1 map

---
+++ PlatformRunner/Assets/Scripts/UIManager/SpringUIManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/*
+ * Spring Scene의 UI 스크립트
+ */
+public class SpringUIManager : MonoBehaviour
+{
+    public GameManager manager;
+    public GameObject panel;
+
+    private void Start()
+    {
+        panel.SetActive(false);
+    }
+
+    //퍼즈 버튼
+    public void PauseButton()
+    {
+        panel.SetActive(true);
+        manager.PauseGameStart();
+    }
+
+    //퍼즈 해제 버튼
+    public void PauseEndButton()
+    {
+        panel.SetActive(false);
+        manager.PauseGameEnd();
+    }
+
+    //재시작 버튼
+    public void RestartButton()
+    {
+        SceneManager.LoadScene("Spring");
+        manager.PauseGameEnd();
+    }
+
+    //뒤로가기 버튼
+    public void BackButton()
+    {
+        SceneManager.LoadScene("Stage");
+        manager.SetCurrentSceneName("Stage");
+        manager.PauseGameEnd();
+    }
+}
