c648da320f2fa25d474e85cfb8017431992d5e5f
cheonjaewoong <cheonjaewoong@gmail.com>
2019-10-20 16:47:08 +0900

    Delete all UI

--- PlatformRunner/Assets/Scripts/GameManager.cs
+++ PlatformRunner/Assets/Scripts/GameManager.cs
@@ -11,13 +11,7 @@ public class GameManager : MonoBehaviour
     public static GameManager instance;
 
     //게임 진행 관리 변수
-    private bool isGaming;  //게임 진행여부
-    private string curruntSceneName;    //현재 씬 이름
-
-    //UI 오브젝트
-    public GameObject titlePanel;
-    public GameObject quitPanel;
-    public GameObject stagePanel;
+    private string curruntSceneName;
     
     private void Awake()
     {
@@ -38,20 +32,7 @@ public class GameManager : MonoBehaviour
 
     private void Update()
     {
-        //뒤로가기 버튼 입력시
-        if (Input.GetKeyDown(KeyCode.Escape))
-        {
-            //현재 Scene에 따라서 다르게 행동
-            switch (curruntSceneName)
-            {
-                //Title Scene에서는 게임종료 패널이 뜨도록
-                case "Title":
-                    QuitButton();
-                    break;
-                default:
-                    break;
-            }
-        }
+        
     }
 
 
@@ -64,12 +45,7 @@ public class GameManager : MonoBehaviour
     public void StartGame()
     {
         //변수 초기화
-        isGaming = true;
         curruntSceneName = "Title";
-
-        //UI 활성화 비활성화 설정
-        quitPanel.SetActive(false);
-        stagePanel.SetActive(false);
     }
 
     //게임 중지시 작동하는 함수
@@ -91,67 +67,6 @@ public class GameManager : MonoBehaviour
     {
         //게임 종료
         Application.Quit();
-    }
-
-    //게임 씬 이동 함수
-    public void MoveScene(string sceneName)
-    {
-        //해당 씬을 로딩하고 현재 씬 이름을 변경
-        SceneManager.LoadScene(sceneName);
-        curruntSceneName = sceneName;
-
-        //해당 씬에 맞춰서 UI 활성화/비활성화
-        switch (sceneName)
-        {
-            case "Title":
-                titlePanel.SetActive(true);
-                quitPanel.SetActive(false);
-                stagePanel.SetActive(false);
-                break;
-            case "Stage":
-                titlePanel.SetActive(false);
-                quitPanel.SetActive(false);
-                stagePanel.SetActive(true);
-                break;
-        }
-    }
-    //--------------------------------------------------
-
-
-
-    /*
-     * --------------------------------------------------
-     * Title UI에서 작동하는 함수
-     */
-    //게임시작버튼
-    public void StartButton()
-    {
-        //Stage Scene으로 이동
-        MoveScene("Stage");
-    }
-
-    //뒤로가기 버튼
-    public void QuitButton()
-    {
-        quitPanel.SetActive(true);
-    }
-    //--------------------------------------------------
-
-
-
-    /*
-     * --------------------------------------------------
-     * Quit UI에서 작동하는 함수
-     */
-    //예 버튼
-    public void QuitYesButton()
-    {
-        QuitGame();
-    }
-    //아니오 버튼
-    public void QuitNoButton()
-    {
-        quitPanel.SetActive(false);
     }
     //--------------------------------------------------
 }
